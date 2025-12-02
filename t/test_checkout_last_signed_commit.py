82c095b802fd897e4b62bace7dc02e9b13928f63
Oliver Kurz <okurz@suse.de>
2025-12-02 10:39:17 +0100

    refactor: Format using current ruff rules

--- t/test_checkout_last_signed_commit.py
+++ t/test_checkout_last_signed_commit.py
@@ -7,18 +7,20 @@ from requests.exceptions import HTTPError
 
 
 class Tests(unittest.TestCase):
-
     def setUp(self):
         self.monkey_patch = pytest.MonkeyPatch()
 
     def test_git_lab_gpg_key_fetcher_uninitialized(self):
-        self.monkey_patch.delenv(name='PRIVATE_TOKEN', raising=False)
+        self.monkey_patch.delenv(name="PRIVATE_TOKEN", raising=False)
         with pytest.raises(SystemExit) as exc_info:
             gpg_key_fetcher = checkout_last_signed_commit.GitLabGPGKeyFetcher()
         assert exc_info.type == SystemExit
-        assert exc_info.value.code == 'Please set the environment variable PRIVATE_TOKEN for GitLab User API Authentication'
+        assert (
+            exc_info.value.code
+            == "Please set the environment variable PRIVATE_TOKEN for GitLab User API Authentication"
+        )
 
-        self.monkey_patch.setenv(name='PRIVATE_TOKEN', value='my_temporary_value')
+        self.monkey_patch.setenv(name="PRIVATE_TOKEN", value="my_temporary_value")
         gpg_key_fetcher = checkout_last_signed_commit.GitLabGPGKeyFetcher()
         assert gpg_key_fetcher.user_api_url == checkout_last_signed_commit.GITLAB_USER_API_DEFAULT
         assert gpg_key_fetcher.user_email is None
@@ -38,12 +40,15 @@ class Tests(unittest.TestCase):
         with pytest.raises(SystemExit) as exc_info:
             commit_checker.init_or_load_repo()
         assert exc_info.type == SystemExit
-        assert exc_info.value.code == f'No previous git checkout at {commit_checker.path_to_checkout_dir} and no URL provided'
+        assert (
+            exc_info.value.code
+            == f"No previous git checkout at {commit_checker.path_to_checkout_dir} and no URL provided"
+        )
         assert commit_checker.get_default_remote_branch() is None
         assert commit_checker.get_commiter_email() == []
         assert commit_checker.get_signed_commit_sha() is None
 
-    @patch('requests.get')
+    @patch("requests.get")
     def test_git_lab_gpg_key_fetcher_initialized(self, mock_get):
         self.monkey_patch.setenv('PRIVATE_TOKEN', 'my_temporary_value')
 
@@ -82,17 +87,23 @@ class Tests(unittest.TestCase):
         """Test _search_user_ids by **username**"""
         mock_data = [{'id': 300, 'username': 'mockuser'}, {'id': 400, 'username': 'usermock'}]
         mock_response.json.return_value = mock_data
-        uid = gpg_key_fetcher._search_user_ids('mock.user')
-        mock_get.assert_called_with(url='http://api.example.com/users/?search=mock.user',
-                                    headers={'PRIVATE-TOKEN': 'my_temporary_value'}, timeout=10)
+        uid = gpg_key_fetcher._search_user_ids("mock.user")
+        mock_get.assert_called_with(
+            url="http://api.example.com/users/?search=mock.user",
+            headers={"PRIVATE-TOKEN": "my_temporary_value"},
+            timeout=10,
+        )
         assert uid == [300, 400]
 
         """Test _search_user_ids by **Name**"""
         mock_data = [{'id': 500, 'username': 'Fake User'}, {'id': 600, 'username': 'johndoe'}]
         mock_response.json.return_value = mock_data
-        uid = gpg_key_fetcher._fetch_user_uid_by_name('Fake User')
-        mock_get.assert_called_with(url='http://api.example.com/users/?search=Fake User',
-                                    headers={'PRIVATE-TOKEN': 'my_temporary_value'}, timeout=10)
+        uid = gpg_key_fetcher._fetch_user_uid_by_name("Fake User")
+        mock_get.assert_called_with(
+            url="http://api.example.com/users/?search=Fake User",
+            headers={"PRIVATE-TOKEN": "my_temporary_value"},
+            timeout=10,
+        )
         assert uid == [500, 600]
 
         """Test _search_user_ids by firstname.lastname extracted from email part"""
@@ -106,10 +117,13 @@ class Tests(unittest.TestCase):
         """Test _search_user_ids for empty response with error status as HTTP response"""
         mock_response.status_code = 500
         mock_response.raise_for_status = Mock()
-        mock_response.raise_for_status.side_effect = HTTPError('gitlab is down')
-        uid = gpg_key_fetcher._fetch_user_uid_by_name('Fake User')
-        mock_get.assert_called_with(url='http://api.example.com/users/?search=Fake User',
-                                    headers={'PRIVATE-TOKEN': 'my_temporary_value'}, timeout=10)
+        mock_response.raise_for_status.side_effect = HTTPError("gitlab is down")
+        uid = gpg_key_fetcher._fetch_user_uid_by_name("Fake User")
+        mock_get.assert_called_with(
+            url="http://api.example.com/users/?search=Fake User",
+            headers={"PRIVATE-TOKEN": "my_temporary_value"},
+            timeout=10,
+        )
         assert uid == []
 
         """Test _search_user_ids for empty return value"""
@@ -126,8 +140,9 @@ class Tests(unittest.TestCase):
         mock_get.return_value = mock_response
 
         gpg_key = gpg_key_fetcher.get_gpg_key_by_uid(uid=500)
-        mock_get.assert_called_with(url='http://api.example.com/users/500/gpg_keys',
-                                    headers={'PRIVATE-TOKEN': 'my_temporary_value'})
+        mock_get.assert_called_with(
+            url="http://api.example.com/users/500/gpg_keys", headers={"PRIVATE-TOKEN": "my_temporary_value"}
+        )
         assert gpg_key == mock_gpg_key
 
         """Test get_gpg_key_by_uid for empty response with error status as HTTP response"""
@@ -149,5 +164,5 @@ class Tests(unittest.TestCase):
         assert gpg_key is None
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
